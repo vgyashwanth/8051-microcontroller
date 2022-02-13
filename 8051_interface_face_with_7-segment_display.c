d11d553f6de94c935d3a1a014fd239a653b4fde6
VIDAPANAKAL GANGAPPAGARI YASHWANTH KUMAR <83589480+vgyashwanth@users.noreply.github.com>
2022-02-13 13:03:17 +0530

    Create 8051_interface_face_with_7-segment_display.c

---
+++ 8051_interface_face_with_7-segment_display.c
@@ -0,0 +1,63 @@
+#include<reg51.h>
+
+void delay(unsigned int k);
+void delay(unsigned int k){
+   unsigned int i,j;
+   for(i=0;i<=k;i++){
+	 for(j=0;j<=12735;j++){}}  } 
+void main(void){
+while(1){
+
+P3=0xBF;
+	delay(5);
+	
+P3=0x06;
+	delay(5);
+	
+	P3=0x5B;
+	delay(5);
+	
+
+
+P3=0x4F;
+	delay(5);
+	
+
+P3=0x66;
+	delay(5);
+	
+P3=0x6D;
+	delay(5);
+	
+
+
+P3=0x7D;
+	delay(5);
+	
+
+P3=0x07;
+	delay(5);
+	
+
+P3=0x7F;
+	delay(5);
+	P3=0x67;
+	delay(5);
+	
+
+
+
+}
+
+
+
+
+
+
+
+
+}		 
+
+
+
+
